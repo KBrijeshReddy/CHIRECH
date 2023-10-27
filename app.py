b33d8e8843dc7d095c67bfb01f943a98ba7ee3ff
AbsoluteReality <89690272+AbsoluteReality@users.noreply.github.com>
2023-10-28 05:26:47 +0530

    a lot

--- app.py
+++ app.py
@@ -1,15 +1,56 @@
 from flask import Flask, render_template, request, url_for, redirect, session
 from flask_pymongo import PyMongo
 
+language = ""
+
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/luxIncognito"
 db = PyMongo(app).db
-
-app.secret_key = "743hfeg7G6wGJRwy"
+app.secret_key = "6g87G77hi8J7h4w7HuF7h"
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/spanish")
+def spanish():
+    global language
+    language = "spanish"
+    return redirect("/questions")
+
+@app.route("/french")
+def french():
+    global language
+    language = "french"
+    return redirect("/questions")
+
+@app.route("/german")
+def german():
+    global language
+    language = "german"
+    return redirect("/questions")
+
+@app.route("/hindi")
+def hindi():
+    global language
+    language = "hindi"
+    return redirect("/questions")
+
+@app.route("/italian")
+def italian():
+    global language
+    language = "italian"
+    return redirect("/questions")
+
+@app.route("/japanese")
+def japanese():
+    global language
+    language = "japanese"
+    return redirect("/questions")
+
+@app.route("/questions", methods=["POST", "GET"])
+def questions():
+    return render_template("questions.html", language=language)
 
 @app.route("/register", methods=["POST", "GET"])
 def register():
@@ -46,4 +87,4 @@ def profile():
         return redirect("/login")
     return render_template("profile.html", name=name, email=db.users.find_one({"name": name}, {"email": 1})["email"])
 
-app.run(debug=True, port=5001)
+app.run(debug=True)
